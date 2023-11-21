bd174c9751836982cc59b3daaa84c4883a09a4dd
Agastya Kommanamanchi <kommanamanchi.agastya@gmail.com>
2023-11-21 16:09:49 -0500

    Update main.py

--- model/main.py
+++ model/main.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.metrics import mean_squared_error
 
 def generate_synthetic_data():
     """Generate synthetic data for house price prediction."""
@@ -36,64 +36,44 @@ def generate_synthetic_data():
 
     return data
 
-
 def evaluate_models(data):
-    """Evaluate different regression models using cross-validation."""
-    # Splitting features and target variable
+    """Evaluate different regression models using cross-validation and return model metrics."""
     X = data.drop('Price', axis=1)
     y = data['Price']
-
-    # Feature scaling using StandardScaler
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
-
-    # Apply PCA for dimensionality reduction
     pca = PCA(n_components=3)
     X_pca = pca.fit_transform(X_scaled)
 
-    # Define models
     models = {
         'Linear Regression': LinearRegression(),
         'Random Forest': RandomForestRegressor(n_estimators=100, random_state=42),
         'Gradient Boosting': GradientBoostingRegressor(n_estimators=100, random_state=42)
     }
 
-    # Evaluate models using cross-validation
-    scoring = make_scorer(mean_squared_error, greater_is_better=False)
-    results = {}
+    model_metrics = {}
     for model_name, model in models.items():
         if model_name == 'Linear Regression':
-            scores = cross_val_score(model, X_scaled, y, cv=5, scoring=scoring)
+            scores = cross_val_score(model, X_scaled, y, cv=5, scoring='neg_mean_squared_error')
         else:
-            scores = cross_val_score(model, X_pca, y, cv=5, scoring=scoring)
-        results[model_name] = scores
+            scores = cross_val_score(model, X_pca, y, cv=5, scoring='neg_mean_squared_error')
+        mse_mean = -scores.mean()
+        model_metrics[model_name] = mse_mean
 
-    # Display mean MSE values for each model
-    for model_name, scores in results.items():
-        print(f"Model: {model_name}")
-        print(f"Mean MSE: {-scores.mean():.2f} (+/- {scores.std():.2f})")
-        print("=" * 40)
-
+    return model_metrics
 
 def visualize_eda(data):
-    """Create EDA visualizations and save them as an image."""
-    # Create pairplot for relationships between variables
+    """Create EDA visualizations and save them as images."""
     sns.pairplot(data)
     plt.title('Pairplot - Relationships between Variables')
     plt.savefig('pairplot.png')  # Save pairplot as an image
     plt.close()
 
-    # Correlation matrix heatmap
     correlation_matrix = data.corr()
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
     plt.title('Correlation Matrix')
     plt.savefig('correlation_heatmap.png')  # Save heatmap as an image
     plt.close()
-
-    # Summary statistics table visualization (optional)
-    summary_table = data.describe()
-    summary_table.to_csv('summary_statistics.csv')  # Save summary statistics as a CSV file
-
 
 if __name__ == "__main__":
     # Generate synthetic data
@@ -102,5 +82,12 @@ if __name__ == "__main__":
     # Visualize EDA
     visualize_eda(house_data)
 
-    # Evaluate models
-    evaluate_models(house_data)
+    # Evaluate models and get model metrics
+    model_metrics = evaluate_models(house_data)
+
+    # Display model metrics
+    print("## Model Metrics")
+    print("| Model                 | Mean MSE   |")
+    print("|-----------------------|------------|")
+    for model_name, mse_mean in model_metrics.items():
+        print(f"| {model_name.ljust(23)} | {mse_mean:.2f}    |")
